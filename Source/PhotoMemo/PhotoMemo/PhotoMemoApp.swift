9eecbc8c606e31e5fd2b4efacbcb67c1afbcdca7
serydo <serydoo@gmail.com>
2026-06-16 17:40:37 +0800

    Fix Xcode project tracking

---
+++ Source/PhotoMemo/PhotoMemo/PhotoMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoMemoApp.swift
+//  PhotoMemo
+//
+//  Created by 汪瑞 on 2026/6/16.
+//
+
+import SwiftUI
+
+@main
+struct PhotoMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
